b1f1a43c43c09479dbe40995d4eab839d03db257
Finn Böger <finnboeger@googlemail.com>
2023-06-17 22:27:31 +0200

    fix device information instantiation

--- main.py
+++ main.py
@@ -279,12 +279,13 @@ if __name__ == "__main__":
         bus = can.Bus('can0', interface='socketcan')
         notifier = can.Notifier(bus, [])
 
-        device_information = n2k.DeviceInformation()
-        device_information.unique_number = 1
-        device_information.device_function = 130
-        device_information.device_class = 25
-        device_information.manufacturer_code = 2046
-        device_information.industry_group = 4
+        device_information = n2k.DeviceInformation(
+            unique_number=1,
+            device_function=130,
+            device_class=25,
+            manufacturer_code=2046,
+            industry_group=4,
+        )
 
         n2k_node = n2k.Node(bus, device_information)
         n2k_node.set_product_information("Test", "0.0.1", "Dev", "00000000001", 5)
