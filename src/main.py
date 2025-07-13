97104b2b07dd8ab29ceb8c3d681c62ce3ccd4d30
Finn Böger <finnboeger@googlemail.com>
2025-07-13 12:24:15 +0200

    move position forwarding code to nmea.py

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ import logging.handlers
 import threading
 from queue import Queue
 
-import n2k
-
 import gps
 import nmea
 import wind
@@ -46,72 +44,6 @@ def init_logging() -> None:
         root_logger.addHandler(handler)
 
 
-def forward_position(
-    n2k_node: n2k.Node,
-    input_queue: Queue[PositionData],
-    output_queue: Queue[PositionData],
-) -> None:
-    """
-    Forward position data from the input to the NMEA2000 network and the output queue.
-
-    Note: The Garmin display GMI20 only displays the GPS data if the COG/SOG message
-    is sent as well, and also has both its fields set to a proper value (i.e. not None).
-
-    :param n2k_node: NMEA2000 Node to send messages through.
-    :param input_queue: Input queue containing position data.
-    :param output_queue: Output queue to forward position data.
-    """
-    while True:
-        position = input_queue.get()
-
-        message = n2k.messages.create_n2k_gnss_data_message(
-            n2k.messages.GNSSPositionData(
-                days_since_1970=position.timestamp // 86400,  # 86400 seconds in a day
-                seconds_since_midnight=position.timestamp % 86400,
-                latitude=position.latitude,
-                longitude=position.longitude,
-                altitude=position.altitude,
-                gnss_type=n2k.types.N2kGNSSType.GPS,
-                gnss_method=n2k.types.N2kGNSSMethod.GNSS_fix,
-                n_satellites=position.number_satellites_used,
-                hdop=position.horizontal_dilution_of_precision,
-                pdop=position.positional_dilution_of_precision,
-                geoidal_separation=position.geoidal_separation,
-                n_reference_stations=None,
-                reference_station_type=None,
-                reference_station_id=position.differential_reference_station_id,
-                age_of_correction=position.differential_gps_data_age,
-            ),
-        )
-        n2k_node.send_msg(message)
-
-        message = n2k.messages.create_n2k_lat_long_rapid_message(
-            n2k.messages.LatLonRapid(
-                latitude=position.latitude,
-                longitude=position.longitude,
-            ),
-        )
-        n2k_node.send_msg(message)
-
-        message = n2k.messages.create_n2k_cog_sog_rapid_message(
-            n2k.messages.CogSogRapid(
-                cog=(
-                    n2k.utils.deg_to_rad(position.true_course)
-                    if position.true_course is not None
-                    # Default to 0 if None, as the Display won't show the data otherwise
-                    else 0.0
-                ),
-                sog=n2k.utils.knots_to_meters_per_second(position.speed)
-                if position.speed is not None
-                else 0.0,
-                heading_reference=n2k.types.N2kHeadingReference.true,
-            ),
-        )
-        n2k_node.send_msg(message)
-
-        output_queue.put(position)
-
-
 if __name__ == "__main__":
     init_logging()
 
@@ -122,7 +54,7 @@ if __name__ == "__main__":
 
     # Forward the GPS data to the NMEA2000 network
     threading.Thread(
-        target=forward_position,
+        target=nmea.forward_position,
         args=(n2k_node, position_queue, position_queue2),
         daemon=True,
     ).start()
